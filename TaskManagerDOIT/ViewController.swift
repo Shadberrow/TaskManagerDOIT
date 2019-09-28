7cf68258dbc93fbcb5fadf58141550d6381ff6dd
Yevhenii <veret.evg@gmail.com>
2019-09-28 10:34:40 +0900

    initial project commit

---
+++ TaskManagerDOIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskManagerDOIT
+//
+//  Created by Yevhenii Veretennikov on 9/28/19.
+//  Copyright © 2019 Yevhenii Veretennikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
